51a98ed59828283428323ac0f860e7302d1aeea2
Nanouq <nanouq@hotmail.com>
2024-10-19 13:40:54 -0400

    Initial commit

---
+++ PantryApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PantryApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
